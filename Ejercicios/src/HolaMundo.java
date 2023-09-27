674f60e59fc3c312556ebcc461fb37ba053a80a6
jArtetxe <145749032+jArtetxe@users.noreply.github.com>
2023-09-27 08:33:42 +0200

    Update HolaMundo.java

--- Ejercicios/src/HolaMundo.java
+++ Ejercicios/src/HolaMundo.java
@@ -2,7 +2,7 @@
 public class HolaMundo {
 	private static void main(String args[]) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo!!, editado desde la web");
 
 	}
 }
